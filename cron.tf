5fb51a50f57fc3c40313a4d70a00e91a6cc11e3a
Jiayu Yi <yijiayu@gmail.com>
2020-01-08 22:14:12 +0800

    Schedule lambda function with CloudWatch Events

---
+++ cron.tf
@@ -0,0 +1,17 @@
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.checkup.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.schedule_checkups.arn
+}
+
+resource "aws_cloudwatch_event_rule" "schedule_checkups" {
+  name = "schedule-checkups-for-${replace(var.url, ".", "-dot-")}"
+
+  schedule_expression = "rate(${var.checkup_interval})"
+}
+
+resource "aws_cloudwatch_event_target" "run_checkups" {
+  arn  = aws_lambda_function.checkup.arn
+  rule = aws_cloudwatch_event_rule.schedule_checkups.name
+}
--- variables.tf
+++ variables.tf
@@ -6,3 +6,8 @@ variable "aws_region" {
   type    = string
   default = "ap-southeast-1"
 }
+
+variable "checkup_interval" {
+  type    = string
+  default = "5 minutes"
+}
